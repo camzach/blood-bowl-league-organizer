9043ab1f0af0d8a0e2fa1339465465ce1a18c5d9
Cameron Zach <czach@sevone.com>
2022-04-30 11:47:59 -0400

    Add team value field to teams

--- backend/src/resolvers/team.ts
+++ backend/src/resolvers/team.ts
@@ -1,6 +1,8 @@
 import type { QueryResolvers, TeamResolvers } from '../graphql.gen';
 import teams from '../teams.json';
 import players from '../players.json';
+import rosters from '../rosters.json';
+import { getPlayerValue } from './utils';
 
 export const TeamQueries: QueryResolvers = {
   team: (parent, query) => {
@@ -15,5 +17,19 @@ export const Team: TeamResolvers = {
   players: parent => {
     const teamPlayers = players.filter(p => parent.playerIds.includes(p.id));
     return teamPlayers;
+  },
+  teamValue: parent => {
+    const playerValues = players
+      .filter(player => parent.playerIds.includes(player.id))
+      .reduce((prev, player) => {
+        const { base, current } = getPlayerValue(player);
+        return { base: base + prev.base, current: current + prev.current };
+      }, { base: 0, current: 0 });
+    const roster = rosters[parent.race as keyof typeof rosters];
+    const staffValue =
+      (parent.apothecary ? 50000 : 0) +
+      ((parent.cheerleaders + parent.coaches) * 10000) +
+      (parent.rerolls * roster.rerollCost);
+    return { base: playerValues.base + staffValue, current: playerValues.current + staffValue };
   },
 };
