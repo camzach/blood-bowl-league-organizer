2cc49127235091adaf978bfcbe5380dfe6eead68
camzach <22503898+camzach@users.noreply.github.com>
2022-04-30 11:47:59 -0400

    Add team value field to teams

--- backend/src/resolvers/player.ts
+++ backend/src/resolvers/player.ts
@@ -1,16 +1,9 @@
-import type { PlayerResolvers, Player as PlayerType, QueryResolvers } from '../graphql.gen';
+import type { PlayerResolvers, QueryResolvers } from '../graphql.gen';
 import teams from '../teams.json';
 import players from '../players.json';
 import rosters from '../rosters.json';
 import type { PlayerModel } from '../models';
-
-function getBasePlayer(player: PlayerModel): (typeof rosters)[keyof (typeof rosters)]['players'][number] {
-  const basePlayer = Object.values(rosters)
-    .flatMap(roster => roster.players)
-    .find(p => p.position === player.position);
-  if (!basePlayer) throw new Error('Player position not recognized');
-  return basePlayer;
-}
+import { getBasePlayer, getPlayerValue } from './utils';
 
 export const Player: PlayerResolvers = {
   team: parent => {
@@ -37,39 +30,7 @@ export const Player: PlayerResolvers = {
     if (!basePlayer) throw new Error('Player position not recognized.');
     return [...basePlayer.skills];
   },
-  teamValue: (parent): PlayerType['teamValue'] => {
-    const basePlayer = getBasePlayer(parent);
-    let base = basePlayer.cost;
-    for (const progression of parent.progression) {
-      switch (progression) {
-        case 'AV':
-        case 'Random Primary':
-          base += 10000;
-          break;
-        case 'MA':
-        case 'PA':
-        case 'Chosen Primary':
-        case 'Random Secondary':
-          base += 20000;
-          break;
-        case 'Chosen Secondary':
-        case 'AG':
-          base += 40000;
-          break;
-        case 'ST':
-          base += 80000;
-          break;
-      }
-      base += 0 as never;
-    }
-    const baseTeam = Object.values(rosters).find(roster =>
-      roster.players.some(player => player.position === parent.position));
-    if (!baseTeam) throw new Error('Player position not recognized');
-    const noCurrentCost =
-      (baseTeam.specialRules.includes('Low Cost Linemen') && basePlayer.max >= 12) ||
-      parent.injuries.missNextGame;
-    return { base, current: noCurrentCost ? 0 : base };
-  },
+  teamValue: getPlayerValue,
   casualties: parent => ({ missNextGame: parent.injuries.missNextGame, niggles: parent.injuries.niggles }),
 };
 
---
+++ backend/src/resolvers/utils.ts
@@ -0,0 +1,45 @@
+import type { Player } from '../graphql.gen';
+import rosters from '../rosters.json';
+import type { PlayerModel } from '../models';
+
+export function getBasePlayer(player: PlayerModel): (typeof rosters)[keyof (typeof rosters)]['players'][number] {
+  const basePlayer = Object.values(rosters)
+    .flatMap(roster => roster.players)
+    .find(p => p.position === player.position);
+  if (!basePlayer) throw new Error('Player position not recognized');
+  return basePlayer;
+}
+
+export function getPlayerValue(parent: PlayerModel): Player['teamValue'] {
+  const basePlayer = getBasePlayer(parent);
+  let base = basePlayer.cost;
+  for (const progression of parent.progression) {
+    switch (progression) {
+      case 'AV':
+      case 'Random Primary':
+        base += 10000;
+        break;
+      case 'MA':
+      case 'PA':
+      case 'Chosen Primary':
+      case 'Random Secondary':
+        base += 20000;
+        break;
+      case 'Chosen Secondary':
+      case 'AG':
+        base += 40000;
+        break;
+      case 'ST':
+        base += 80000;
+        break;
+    }
+    base += 0 as never;
+  }
+  const baseTeam = Object.values(rosters).find(roster =>
+    roster.players.some(player => player.position === parent.position));
+  if (!baseTeam) throw new Error('Player position not recognized');
+  const noCurrentCost =
+    (baseTeam.specialRules.includes('Low Cost Linemen') && basePlayer.max >= 12) ||
+    parent.injuries.missNextGame;
+  return { base, current: noCurrentCost ? 0 : base };
+}
