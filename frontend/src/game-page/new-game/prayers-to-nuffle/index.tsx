0f2d829eda59e0dda605d9aa1992371e42bb9f0a
Cameron Zach <czach@sevone.com>
2022-05-11 23:26:43 -0400

    Add prayers to nuffle page

---
+++ frontend/src/game-page/new-game/prayers-to-nuffle/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { gameContext } from '..';
+
+export function PrayersToNuffle(): React.ReactElement {
+  const { gameInfo: { home, away }, dispatch } = React.useContext(gameContext);
+  const handleResult = React.useCallback(() => {
+    dispatch({ type: 'prayersToNuffle' });
+  }, [dispatch]);
+  const prayersHome = Math.max(0, Math.floor((away.currentTeamValue - home.currentTeamValue) / 50_000));
+  const prayersAway = Math.max(0, Math.floor((home.currentTeamValue - away.currentTeamValue) / 50_000));
+  return <button type="button" onClick={handleResult}>{prayersHome} - {prayersAway}</button>;
+}
