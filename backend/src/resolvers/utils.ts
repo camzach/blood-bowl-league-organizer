665bcc340a6498d005431372cdcfd92472023cef
camzach <22503898+camzach@users.noreply.github.com>
2022-05-20 10:01:47 -0400

    Fix low-cost linemen calculation

--- backend/src/resolvers/utils.ts
+++ backend/src/resolvers/utils.ts
@@ -4,33 +4,32 @@ import type { PlayerDbObject, RosterDbObject, SkillDbObject, TeamValue } from '.
 export function getPlayerValue(parent: PlayerDbObject, roster: RosterDbObject): TeamValue {
   const basePlayer = roster.players.find(p => p.position === parent.position);
   if (!basePlayer) throw new Error('Player position not recognized');
-  let base = basePlayer.cost;
+  const noHiringFee = (roster.specialRules.includes('Low Cost Linemen') && basePlayer.max >= 12);
+  let cost = noHiringFee ? basePlayer.cost : 0;
   for (const progression of parent.progression) {
     switch (progression) {
       case 'AV':
       case 'Random Primary':
-        base += 10000;
+        cost += 10000;
         break;
       case 'MA':
       case 'PA':
       case 'Chosen Primary':
       case 'Random Secondary':
-        base += 20000;
+        cost += 20000;
         break;
       case 'Chosen Secondary':
       case 'AG':
-        base += 40000;
+        cost += 40000;
         break;
       case 'ST':
-        base += 80000;
+        cost += 80000;
         break;
     }
-    base += 0 as never;
+    cost += 0 as never;
   }
-  const noCurrentCost =
-    (roster.specialRules.includes('Low Cost Linemen') && basePlayer.max >= 12) ||
-    parent.injuries.missNextGame;
-  return { base, current: noCurrentCost ? 0 : base };
+
+  return { base: cost, current: parent.injuries.missNextGame ? 0 : cost };
 }
 
 export async function getModifiedSkills(
