86cf312e4ac2167b4c54a9a3b620f16d8d225eac
Cameron Zach <czach@sevone.com>
2022-10-21 17:01:52 -0400

    fire players and staff

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { teamRouter } from './team';
 import { scheduleRouter } from './schedule-generator';
 import { gameRouter } from './game';
+import { playerRouter } from './player';
 
 const app = express();
 const port = process.env.PORT ?? 8080;
@@ -12,6 +13,7 @@ const appRouter = router({
   team: teamRouter,
   schedule: scheduleRouter,
   game: gameRouter,
+  player: playerRouter,
 });
 
 app.use(createExpressMiddleware({ router: appRouter }));
