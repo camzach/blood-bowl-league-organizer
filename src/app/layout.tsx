717247feb324624a2f769e4aa18c9e9aaaa0b571
camzach <22503898+camzach@users.noreply.github.com>
2022-11-22 12:50:28 -0500

    move head to head.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,6 @@ import type { PropsWithChildren, ReactNode } from 'react';
 export default function RootLayout({ children }: PropsWithChildren): ReactNode {
   return (
     <html>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
-      </head>
       <body>
         {children}
       </body>
