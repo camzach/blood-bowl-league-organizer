ed2b6da3796f10d79502643497b03be0abda3d15
camzach <22503898+camzach@users.noreply.github.com>
2023-01-31 14:10:32 -0500

    add basic auth

--- prisma/migrations/20230131185040_init/migration.sql
+++ prisma/migrations/20230131185040_init/migration.sql
@@ -11,33 +11,42 @@ CREATE TYPE "GameState" AS ENUM ('Scheduled', 'Journeymen', 'Inducements', 'InPr
 CREATE TYPE "Weather" AS ENUM ('Blizzard', 'PouringRain', 'Perfect', 'VerySunny', 'BlisteringHeat');
 
 -- CreateTable
+CREATE TABLE "Coach" (
+    "name" STRING NOT NULL,
+    "passwordHash" STRING NOT NULL,
+
+    CONSTRAINT "Coach_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
 CREATE TABLE "Player" (
     "id" UUID NOT NULL,
-    "name" TEXT,
-    "playerTeamName" TEXT,
-    "journeymanTeamName" TEXT,
-    "nigglingInjuries" INTEGER NOT NULL DEFAULT 0,
-    "missNextGame" BOOLEAN NOT NULL DEFAULT false,
-    "dead" BOOLEAN NOT NULL DEFAULT false,
-    "AG" INTEGER NOT NULL,
-    "MA" INTEGER NOT NULL,
-    "PA" INTEGER,
-    "ST" INTEGER NOT NULL,
-    "AV" INTEGER NOT NULL,
-    "AGImprovements" INTEGER NOT NULL DEFAULT 0,
-    "MAImprovements" INTEGER NOT NULL DEFAULT 0,
-    "PAImprovements" INTEGER NOT NULL DEFAULT 0,
-    "STImprovements" INTEGER NOT NULL DEFAULT 0,
-    "AVImprovements" INTEGER NOT NULL DEFAULT 0,
-    "totalImprovements" INTEGER NOT NULL DEFAULT 0,
-    "starPlayerPoints" INTEGER NOT NULL DEFAULT 0,
-    "touchdowns" INTEGER NOT NULL DEFAULT 0,
-    "completions" INTEGER NOT NULL DEFAULT 0,
-    "deflections" INTEGER NOT NULL DEFAULT 0,
-    "interceptions" INTEGER NOT NULL DEFAULT 0,
-    "casualties" INTEGER NOT NULL DEFAULT 0,
-    "MVPs" INTEGER NOT NULL DEFAULT 0,
-    "teamValue" INTEGER NOT NULL,
+    "name" STRING,
+    "number" INT4 NOT NULL,
+    "playerTeamName" STRING,
+    "journeymanTeamName" STRING,
+    "nigglingInjuries" INT4 NOT NULL DEFAULT 0,
+    "missNextGame" BOOL NOT NULL DEFAULT false,
+    "dead" BOOL NOT NULL DEFAULT false,
+    "AG" INT4 NOT NULL,
+    "MA" INT4 NOT NULL,
+    "PA" INT4,
+    "ST" INT4 NOT NULL,
+    "AV" INT4 NOT NULL,
+    "AGImprovements" INT4 NOT NULL DEFAULT 0,
+    "MAImprovements" INT4 NOT NULL DEFAULT 0,
+    "PAImprovements" INT4 NOT NULL DEFAULT 0,
+    "STImprovements" INT4 NOT NULL DEFAULT 0,
+    "AVImprovements" INT4 NOT NULL DEFAULT 0,
+    "totalImprovements" INT4 NOT NULL DEFAULT 0,
+    "starPlayerPoints" INT4 NOT NULL DEFAULT 0,
+    "touchdowns" INT4 NOT NULL DEFAULT 0,
+    "completions" INT4 NOT NULL DEFAULT 0,
+    "deflections" INT4 NOT NULL DEFAULT 0,
+    "interceptions" INT4 NOT NULL DEFAULT 0,
+    "casualties" INT4 NOT NULL DEFAULT 0,
+    "MVPs" INT4 NOT NULL DEFAULT 0,
+    "teamValue" INT4 NOT NULL,
     "primary" "SkillCategory"[],
     "secondary" "SkillCategory"[],
     "positionId" UUID NOT NULL,
@@ -47,31 +56,32 @@ CREATE TABLE "Player" (
 
 -- CreateTable
 CREATE TABLE "Team" (
-    "name" TEXT NOT NULL,
-    "treasury" INTEGER NOT NULL DEFAULT 1000000,
+    "name" STRING NOT NULL,
+    "treasury" INT4 NOT NULL DEFAULT 1000000,
     "state" "TeamState" NOT NULL DEFAULT 'Draft',
-    "rosterName" TEXT NOT NULL,
-    "rerolls" INTEGER NOT NULL DEFAULT 0,
-    "cheerleaders" INTEGER NOT NULL DEFAULT 0,
-    "assistantCoaches" INTEGER NOT NULL DEFAULT 0,
-    "apothecary" BOOLEAN NOT NULL DEFAULT false,
-    "dedicatedFans" INTEGER NOT NULL DEFAULT 0,
+    "rosterName" STRING NOT NULL,
+    "rerolls" INT4 NOT NULL DEFAULT 0,
+    "cheerleaders" INT4 NOT NULL DEFAULT 0,
+    "assistantCoaches" INT4 NOT NULL DEFAULT 0,
+    "apothecary" BOOL NOT NULL DEFAULT false,
+    "dedicatedFans" INT4 NOT NULL DEFAULT 0,
+    "coachName" STRING,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateTable
 CREATE TABLE "Roster" (
-    "name" TEXT NOT NULL,
-    "rerollCost" INTEGER NOT NULL,
-    "tier" INTEGER NOT NULL,
+    "name" STRING NOT NULL,
+    "rerollCost" INT4 NOT NULL,
+    "tier" INT4 NOT NULL,
 
     CONSTRAINT "Roster_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateTable
 CREATE TABLE "SpecialRule" (
-    "name" TEXT NOT NULL,
+    "name" STRING NOT NULL,
 
     CONSTRAINT "SpecialRule_pkey" PRIMARY KEY ("name")
 );
@@ -79,25 +89,25 @@ CREATE TABLE "SpecialRule" (
 -- CreateTable
 CREATE TABLE "Position" (
     "id" UUID NOT NULL,
-    "name" TEXT NOT NULL,
-    "max" INTEGER NOT NULL,
-    "cost" INTEGER NOT NULL,
-    "MA" INTEGER NOT NULL,
-    "ST" INTEGER NOT NULL,
-    "AG" INTEGER NOT NULL,
-    "PA" INTEGER,
-    "AV" INTEGER NOT NULL,
+    "name" STRING NOT NULL,
+    "max" INT4 NOT NULL,
+    "cost" INT4 NOT NULL,
+    "MA" INT4 NOT NULL,
+    "ST" INT4 NOT NULL,
+    "AG" INT4 NOT NULL,
+    "PA" INT4,
+    "AV" INT4 NOT NULL,
     "primary" "SkillCategory"[],
     "secondary" "SkillCategory"[],
-    "rosterName" TEXT NOT NULL,
+    "rosterName" STRING NOT NULL,
 
     CONSTRAINT "Position_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Skill" (
-    "name" TEXT NOT NULL,
-    "rules" TEXT NOT NULL,
+    "name" STRING NOT NULL,
+    "rules" STRING NOT NULL,
     "category" "SkillCategory" NOT NULL,
 
     CONSTRAINT "Skill_pkey" PRIMARY KEY ("name")
@@ -105,38 +115,38 @@ CREATE TABLE "Skill" (
 
 -- CreateTable
 CREATE TABLE "Inducement" (
-    "name" TEXT NOT NULL,
-    "max" INTEGER NOT NULL,
-    "price" INTEGER,
-    "specialPriceRuleName" TEXT,
-    "specialPrice" INTEGER,
-    "rules" TEXT NOT NULL,
+    "name" STRING NOT NULL,
+    "max" INT4 NOT NULL,
+    "price" INT4,
+    "specialPriceRuleName" STRING,
+    "specialPrice" INT4,
+    "rules" STRING NOT NULL,
 
     CONSTRAINT "Inducement_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateTable
 CREATE TABLE "StarPlayer" (
-    "name" TEXT NOT NULL,
-    "MA" INTEGER NOT NULL,
-    "ST" INTEGER NOT NULL,
-    "PA" INTEGER,
-    "AG" INTEGER NOT NULL,
-    "AV" INTEGER NOT NULL,
-    "specialRule" TEXT NOT NULL,
-    "hiringFee" INTEGER NOT NULL,
+    "name" STRING NOT NULL,
+    "MA" INT4 NOT NULL,
+    "ST" INT4 NOT NULL,
+    "PA" INT4,
+    "AG" INT4 NOT NULL,
+    "AV" INT4 NOT NULL,
+    "specialRule" STRING NOT NULL,
+    "hiringFee" INT4 NOT NULL,
 
     CONSTRAINT "StarPlayer_pkey" PRIMARY KEY ("name")
 );
 
 -- CreateTable
 CREATE TABLE "InducementOption" (
-    "name" TEXT NOT NULL,
-    "price" INTEGER,
-    "specialPriceRuleName" TEXT,
-    "specialPrice" INTEGER,
-    "rules" TEXT NOT NULL,
-    "inducementName" TEXT,
+    "name" STRING NOT NULL,
+    "price" INT4,
+    "specialPriceRuleName" STRING,
+    "specialPrice" INT4,
+    "rules" STRING NOT NULL,
+    "inducementName" STRING,
 
     CONSTRAINT "InducementOption_pkey" PRIMARY KEY ("name")
 );
@@ -144,20 +154,20 @@ CREATE TABLE "InducementOption" (
 -- CreateTable
 CREATE TABLE "Game" (
     "id" UUID NOT NULL,
-    "round" INTEGER NOT NULL,
-    "homeTeamName" TEXT NOT NULL,
-    "awayTeamName" TEXT NOT NULL,
+    "round" INT4 NOT NULL,
+    "homeTeamName" STRING NOT NULL,
+    "awayTeamName" STRING NOT NULL,
     "state" "GameState" NOT NULL DEFAULT 'Scheduled',
-    "journeymenHome" INTEGER NOT NULL DEFAULT 0,
-    "journeymenAway" INTEGER NOT NULL DEFAULT 0,
-    "pettyCashHome" INTEGER NOT NULL DEFAULT 0,
-    "pettyCashAway" INTEGER NOT NULL DEFAULT 0,
-    "touchdownsHome" INTEGER NOT NULL DEFAULT 0,
-    "touchdownsAway" INTEGER NOT NULL DEFAULT 0,
-    "casualtiesHome" INTEGER NOT NULL DEFAULT 0,
-    "casualtiesAway" INTEGER NOT NULL DEFAULT 0,
-    "fanFacotrHome" INTEGER NOT NULL DEFAULT 0,
-    "fanFactorAway" INTEGER NOT NULL DEFAULT 0,
+    "journeymenHome" INT4 NOT NULL DEFAULT 0,
+    "journeymenAway" INT4 NOT NULL DEFAULT 0,
+    "pettyCashHome" INT4 NOT NULL DEFAULT 0,
+    "pettyCashAway" INT4 NOT NULL DEFAULT 0,
+    "touchdownsHome" INT4 NOT NULL DEFAULT 0,
+    "touchdownsAway" INT4 NOT NULL DEFAULT 0,
+    "casualtiesHome" INT4 NOT NULL DEFAULT 0,
+    "casualtiesAway" INT4 NOT NULL DEFAULT 0,
+    "fanFacotrHome" INT4 NOT NULL DEFAULT 0,
+    "fanFactorAway" INT4 NOT NULL DEFAULT 0,
     "weather" "Weather",
 
     CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
@@ -166,32 +176,35 @@ CREATE TABLE "Game" (
 -- CreateTable
 CREATE TABLE "_PlayerToSkill" (
     "A" UUID NOT NULL,
-    "B" TEXT NOT NULL
+    "B" STRING NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_RosterToSpecialRule" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "A" STRING NOT NULL,
+    "B" STRING NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_SpecialRuleToStarPlayer" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "A" STRING NOT NULL,
+    "B" STRING NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_PositionToSkill" (
     "A" UUID NOT NULL,
-    "B" TEXT NOT NULL
+    "B" STRING NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "_SkillToStarPlayer" (
-    "A" TEXT NOT NULL,
-    "B" TEXT NOT NULL
+    "A" STRING NOT NULL,
+    "B" STRING NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Coach_name_key" ON "Coach"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Player_id_key" ON "Player"("id");
@@ -264,6 +277,9 @@ ALTER TABLE "Player" ADD CONSTRAINT "Player_positionId_fkey" FOREIGN KEY ("posit
 
 -- AddForeignKey
 ALTER TABLE "Team" ADD CONSTRAINT "Team_rosterName_fkey" FOREIGN KEY ("rosterName") REFERENCES "Roster"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_coachName_fkey" FOREIGN KEY ("coachName") REFERENCES "Coach"("name") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Position" ADD CONSTRAINT "Position_rosterName_fkey" FOREIGN KEY ("rosterName") REFERENCES "Roster"("name") ON DELETE RESTRICT ON UPDATE CASCADE;
