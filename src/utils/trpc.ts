a81e8c684edd2d18c79fcf45e9a82b5f1324bcfb
camzach <22503898+camzach@users.noreply.github.com>
2023-02-01 16:04:08 -0500

    fix baseurl routing for prod

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -2,6 +2,13 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from 'server/routers';
 
 function getBaseUrl(): string {
+  if (typeof window !== 'undefined')
+    // Use relative path in browser
+    return '';
+  if (process.env.VERCEL_URL !== undefined)
+    // Use env var for Vercel
+    return `https://${process.env.VERCEL_URL}`;
+  // Otherwise, assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
