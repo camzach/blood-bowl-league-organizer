307b090fac54b498921df31364eebd9cdd00082c
camzach <22503898+camzach@users.noreply.github.com>
2023-02-01 17:35:29 -0500

    remove middleware causing trpc to fail

--- src/app/game/[gameId]/layout.tsx
+++ src/app/game/[gameId]/layout.tsx
@@ -1,11 +1,4 @@
 import type { PropsWithChildren, ReactElement } from 'react';
-import { prisma } from 'utils/prisma';
-
-export async function generateStaticParams(): Promise<Array<{ gameId: string }>> {
-  return (await prisma.game.findMany({ select: { id: true } })).map(({ id }) => ({ gameId: id }));
-}
-
-export const dynamicParams = false;
 
 export default function Layout({ children }: PropsWithChildren): ReactElement {
   return <>{children}</>;
