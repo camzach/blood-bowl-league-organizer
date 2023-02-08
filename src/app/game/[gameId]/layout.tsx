0a68258c4ca129c5501ce8522bb57f7509a214dd
camzach <22503898+camzach@users.noreply.github.com>
2023-02-08 13:30:33 -0500

    couple of quick tweaks

--- src/app/game/[gameId]/layout.tsx
+++ src/app/game/[gameId]/layout.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren, ReactElement } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default function Layout({ children }: PropsWithChildren): ReactElement {
   return <>{children}</>;
 }
