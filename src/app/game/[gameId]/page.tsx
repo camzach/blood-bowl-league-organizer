1c93b0367484d04ad97516ac5735fb39321bd45b
camzach <22503898+camzach@users.noreply.github.com>
2023-03-06 23:11:52 -0500

    update completed game page

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -3,12 +3,27 @@ import type { ReactNode } from 'react';
 import { prisma } from 'utils/prisma';
 
 export default async function Game({ params: { gameId } }: { params: { gameId: string } }): Promise<ReactNode> {
-  const data = await prisma.game.findUnique({ where: { id: decodeURIComponent(gameId) } });
-  if (!data)
+  const game = await prisma.game.findUnique({ where: { id: decodeURIComponent(gameId) }, include: { MVPs: true } });
+  if (!game)
     return notFound();
 
-  if (data.state !== 'Complete')
-    redirect(`/game/${gameId}/${data.state.toLowerCase()}`);
+  if (game.state !== 'Complete')
+    redirect(`/game/${gameId}/${game.state.toLowerCase()}`);
 
-  return <>Game ID: {gameId}</>;
+  const [homeMVP, awayMVP] = [game.homeTeamName, game.awayTeamName]
+    .map(team => game.MVPs.find(p => [p.playerTeamName, p.journeymanTeamName].includes(team)));
+
+  return <div>
+    Score:
+    <span>{game.touchdownsHome} - {game.touchdownsAway}</span>
+    <br/>
+    Casualties:
+    <span>{game.casualtiesHome} - {game.casualtiesAway}</span>
+    <br/>
+    MVPs:
+    <br/>
+    <span>Home: {homeMVP?.name ?? homeMVP?.number ?? 'None'}</span>
+    <br/>
+    <span>Away: {awayMVP?.name ?? awayMVP?.number ?? 'None'}</span>
+  </div>;
 }
