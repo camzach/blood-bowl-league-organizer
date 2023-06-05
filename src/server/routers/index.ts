bb82ccd3bfbfda10bc264ac2c6f9b42c013812b4
camzach <22503898+camzach@users.noreply.github.com>
2023-06-05 18:04:09 -0400

    migrate game router to server actions

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,16 +1,12 @@
 import { router } from "server/trpc";
-import { gameRouter } from "./game";
 import { inducementRouter } from "./inducement";
 import { scheduleRouter } from "./schedule-generator";
 import { teamRouter } from "./team";
-import { skillRouter } from "./skill";
 
 export const appRouter = router({
   team: teamRouter,
-  game: gameRouter,
   schedule: scheduleRouter,
   inducements: inducementRouter,
-  skill: skillRouter,
 });
 
 // Export type definition of API
