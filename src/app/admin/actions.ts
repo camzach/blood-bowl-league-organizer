ff013681b4d5f820efab3013cb208a81431f9683
camzach <22503898+camzach@users.noreply.github.com>
2023-12-05 16:14:02 -0500

    admin panel

---
+++ src/app/admin/actions.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { game, gameDetails, roundRobinGame, season } from "db/schema";
+import { count } from "drizzle-orm";
+import nanoid from "utils/nanoid";
+import { db } from "utils/drizzle";
+import { action } from "utils/safe-action";
+import { generateSchedule } from "utils/schedule-generator";
+import { z } from "zod";
+
+export const scheduleAction = action(z.any(), async () => {
+  await db.transaction(async (tx) => {
+    const games = await tx.select({ count: count() }).from(game);
+    if (games[0].count > 0) throw new Error("Schedule already generated");
+
+    const teams = (
+      await tx.query.team.findMany({ columns: { name: true } })
+    ).map((t) => t.name);
+
+    const rounds = generateSchedule(teams).map((round, i) => ({
+      number: i + 1,
+      pairs: round.map((pair) => ({
+        home: { teamName: pair[0], id: nanoid() },
+        away: { teamName: pair[1], id: nanoid() },
+      })),
+    }));
+    await tx
+      .insert(season)
+      .values([{ name: "2024" }])
+      .onConflictDoNothing();
+    for (const round of rounds) {
+      for (const roundGame of round.pairs) {
+        await tx.insert(gameDetails).values([roundGame.home, roundGame.away]);
+        const gameId = nanoid();
+        await tx.insert(game).values([
+          {
+            homeDetailsId: roundGame.home.id,
+            awayDetailsId: roundGame.away.id,
+            id: gameId,
+          },
+        ]);
+        await tx.insert(roundRobinGame).values([
+          {
+            gameId,
+            round: round.number,
+            seasonName: "2024",
+          },
+        ]);
+      }
+    }
+  });
+});
--- src/utils/schedule-generator.ts
+++ src/utils/schedule-generator.ts
@@ -1,20 +1,19 @@
-type PairingsType = [string, string][];
-
-export function generateSchedule(teams: string[]): PairingsType {
+export function generateSchedule(teams: string[]) {
   const localTeams: Array<string | null> = [...teams];
-  const pairings: [string, string][] = [];
+  const rounds: [string, string][][] = [];
   const homeAwayCounts = Object.fromEntries(
-    localTeams.map((team) => [team, [0, 0]])
+    localTeams.map((team) => [team, [0, 0]]),
   ) as Record<string, [number, number]>;
   if (localTeams.length % 2 === 1) localTeams.push(null);
 
   const teamCount = localTeams.length;
-  const rounds = teamCount - 1;
+  const numRounds = teamCount - 1;
   const half = teamCount / 2;
 
   const teamIndices = localTeams.map((_, i) => i).slice(1);
 
-  for (let round = 0; round < rounds; round++) {
+  for (let round = 0; round < numRounds; round++) {
+    const thisRound: [string, string][] = [];
     const newTeamIndices = [0].concat(teamIndices);
 
     const firstHalf = newTeamIndices.slice(0, half);
@@ -31,20 +30,23 @@ export function generateSchedule(teams: string[]): PairingsType {
       }
       homeAwayCounts[home][0] += 1;
       homeAwayCounts[away][1] += 1;
-      pairings.push([home, away]);
+      thisRound.push([home, away]);
     }
 
+    rounds.push(thisRound);
     // Rotating the array
     teamIndices.push(teamIndices.shift() as number);
   }
 
+  const pairings = rounds.flat();
+
   const isScheduleBalanced = (): boolean =>
     Object.values(homeAwayCounts).every(
       ([home, away]) =>
-        Math.abs(home - away) === (localTeams.includes(null) ? 0 : 1)
+        Math.abs(home - away) === (localTeams.includes(null) ? 0 : 1),
     );
 
-  const switchHomeAway = (game: (typeof pairings)[number]): void => {
+  const switchHomeAway = (game: [string, string]): void => {
     const [home, away] = game;
     game.reverse();
     homeAwayCounts[home][0] -= 1;
@@ -57,31 +59,31 @@ export function generateSchedule(teams: string[]): PairingsType {
     if (localTeams.includes(null)) {
       const [tooManyHome] =
         Object.entries(homeAwayCounts).find(
-          ([_, [home, away]]) => home - away > 0
+          ([_, [home, away]]) => home - away > 0,
         ) ?? ([] as never[]);
       const [tooManyAway] =
         Object.entries(homeAwayCounts).find(
-          ([_, [home, away]]) => away - home > 0
+          ([_, [home, away]]) => away - home > 0,
         ) ?? ([] as never[]);
       const gameToFix = pairings.find(
-        ([home, away]) => home === tooManyHome && away === tooManyAway
+        ([home, away]) => home === tooManyHome && away === tooManyAway,
       );
       if (gameToFix) {
         switchHomeAway(gameToFix);
         return;
       }
-      let gameA = null;
-      let gameB = null;
+      let gameA: [string, string] | undefined = undefined;
+      let gameB: [string, string] | undefined = undefined;
       for (const intermediary of localTeams) {
         gameA = pairings.find(
-          ([home, away]) => home === tooManyHome && away === intermediary
+          ([home, away]) => home === tooManyHome && away === intermediary,
         );
         gameB = pairings.find(
-          ([home, away]) => away === tooManyAway && home === intermediary
+          ([home, away]) => away === tooManyAway && home === intermediary,
         );
         if (!gameA || !gameB) {
-          gameA = null;
-          gameB = null;
+          gameA = undefined;
+          gameB = undefined;
         } else {
           break;
         }
@@ -95,7 +97,7 @@ export function generateSchedule(teams: string[]): PairingsType {
       // This will always find a pairing
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const [tooManyHome] = Object.entries(homeAwayCounts).find(
-        ([_, [home, away]]) => home - away > 1
+        ([_, [home, away]]) => home - away > 1,
       )!;
       if (tooManyHome !== null) {
         const game =
@@ -104,7 +106,7 @@ export function generateSchedule(teams: string[]): PairingsType {
           pairings.find(
             ([home, away]) =>
               home === tooManyHome &&
-              homeAwayCounts[away][1] > homeAwayCounts[away][0]
+              homeAwayCounts[away][1] > homeAwayCounts[away][0],
           )!;
         switchHomeAway(game);
         return;
@@ -113,7 +115,7 @@ export function generateSchedule(teams: string[]): PairingsType {
         // This will always find a pairing
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         Object.entries(homeAwayCounts).find(
-          ([_, [home, away]]) => away - home > 1
+          ([_, [home, away]]) => away - home > 1,
         )!;
       const game =
         // This will always find a pairing
@@ -121,7 +123,7 @@ export function generateSchedule(teams: string[]): PairingsType {
         pairings.find(
           ([home, away]) =>
             home === tooManyAway &&
-            homeAwayCounts[away][1] > homeAwayCounts[away][0]
+            homeAwayCounts[away][1] > homeAwayCounts[away][0],
         )!;
       switchHomeAway(game);
     }
@@ -129,17 +131,5 @@ export function generateSchedule(teams: string[]): PairingsType {
 
   while (!isScheduleBalanced()) rebalance();
 
-  return pairings;
+  return rounds;
 }
-
-// export const scheduleRouter = router({
-//   generate: publicProcedure.mutation(async ({ ctx }) => {
-//     const games = await ctx.prisma.game.count();
-//     if (games > 0) throw new Error("Schedule already generated");
-//     const teams = (
-//       await ctx.prisma.team.findMany({ select: { name: true } })
-//     ).map((t) => t.name);
-//     const pairings = generateSchedule(teams);
-//     return ctx.prisma.game.createMany({ data: pairings });
-//   }),
-// });
