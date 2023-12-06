865cf17496217b4b3c9fc2782da74307d958a45f
camzach <22503898+camzach@users.noreply.github.com>
2023-12-06 15:34:40 -0500

    mobile-first navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   const { userId } = auth();
   const user: { publicMetadata: { isAdmin?: boolean } } | null =
     await currentUser();
+
+  // TODO: investigate useId issue
+  const drawerId = "_drawer_";
+
   if (!userId) return <RedirectToSignIn />;
 
   const myTeam = await db.query.coachToTeam.findFirst({
@@ -32,37 +36,71 @@ export default async function RootLayout({ children }: PropsWithChildren) {
     <ClerkProvider>
       <html data-theme="dark">
         <body>
-          <header className="navbar bg-primary text-primary-content">
-            <h1 className="m-0 inline-block w-min text-4xl">BBLO</h1>
-            <nav className="ml-8 flex gap-8">
-              <Link
-                className="text-2xl"
-                href={`/team/${myTeam?.teamName ?? "new"}`}
-              >
-                Teams
-              </Link>
-              <Link className="text-2xl" href="/schedule">
-                Schedule
-              </Link>
-              <Link className="text-2xl" href="/league-table">
-                League Table
-              </Link>
-              <Link className="text-2xl" href="/playoffs">
-                Playoffs
-              </Link>
-              {user?.publicMetadata.isAdmin && (
-                <Link className="text-2xl" href="/admin">
-                  Admin
-                </Link>
-              )}
-            </nav>
-            <span className="ml-auto">
-              <UserButton />
-            </span>
-          </header>
-          <main className="p-4">{children}</main>
+          <div className="drawer">
+            <input id={drawerId} type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              <header className="navbar">
+                <div className="navbar-start md:hidden">
+                  <label htmlFor={drawerId} className="text-5xl">
+                    ≡
+                  </label>
+                </div>
+                <span className="text-3xl max-sm:navbar-center md:navbar-start">
+                  BBLO
+                </span>
+                <nav className="navbar-center hidden gap-3 md:flex">
+                  <NavLinks
+                    team={myTeam?.teamName ?? "new"}
+                    isAdmin={!!user?.publicMetadata.isAdmin}
+                  />
+                </nav>
+                <span className="navbar-end">
+                  <UserButton />
+                </span>
+              </header>
+              <main className="p-4">{children}</main>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor={drawerId}
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <nav className="menu min-h-full w-fit bg-base-200 p-4 pr-16 text-base-content">
+                <NavLinks
+                  team={myTeam?.teamName ?? "new"}
+                  isAdmin={!!user?.publicMetadata.isAdmin}
+                />
+              </nav>
+            </div>
+          </div>
         </body>
       </html>
     </ClerkProvider>
   );
+}
+
+function NavLinks(props: { team: string; isAdmin: boolean }) {
+  const result = [
+    <Link key="team" className="text-xl" href={`/team/${props.team}`}>
+      Team
+    </Link>,
+    <Link key="schedule" className="text-xl" href="/schedule">
+      Schedule
+    </Link>,
+    <Link key="table" className="text-xl" href="/league-table">
+      League Table
+    </Link>,
+    <Link key="playoffs" className="text-xl" href="/playoffs">
+      Playoffs
+    </Link>,
+  ];
+  if (props.isAdmin) {
+    result.push(
+      <Link key="admin" className="text-xl" href="/admin">
+        Admin
+      </Link>,
+    );
+  }
+  return result;
 }
