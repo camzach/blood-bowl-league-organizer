17eb2b0e3ff8a4579d25465f5862e99aeb4f3c21
camzach <22503898+camzach@users.noreply.github.com>
2023-12-19 15:19:54 -0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,35 @@
-import { authMiddleware, clerkClient } from "@clerk/nextjs";
+import { authMiddleware, clerkClient, redirectToSignIn } from "@clerk/nextjs";
 import { NextResponse, URLPattern } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   async afterAuth(auth, req) {
-    const adminPattern = new URLPattern({ pathname: "/admin" });
+    // Allow access to public routes
+    if (auth.isPublicRoute) {
+      return NextResponse.next();
+    }
 
+    // Handle users who aren't authenticated
+    if (!auth.userId) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    // Protect admin route
+    const adminPattern = new URLPattern({ pathname: "/admin" });
     if (
-      !auth.userId ||
-      (adminPattern.test(req.url) &&
-        !(await clerkClient.users.getUser(auth.userId)).publicMetadata.isAdmin)
+      adminPattern.test(req.url) &&
+      !(await clerkClient.users.getUser(auth.userId)).publicMetadata.isAdmin
     ) {
       return NextResponse.json(
         { error: "User is not an admin" },
         { status: 401 },
       );
     }
+
+    // Allow everything else through
+    return NextResponse.next();
   },
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
 };
