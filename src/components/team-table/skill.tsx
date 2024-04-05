0bf654b99d66857c3f7e52aac02b2dd64ee4a9f4
camzach <22503898+camzach@users.noreply.github.com>
2024-04-05 15:00:23 -0400

    hanging indents for long skills

--- src/components/team-table/skill.tsx
+++ src/components/team-table/skill.tsx
@@ -18,7 +18,7 @@ export default function Skill({ skill }: Props) {
       </a>
       <Tooltip
         clickable
-        className="max-h-64 max-w-xl overflow-auto whitespace-pre-wrap leading-6"
+        className="max-h-64 max-w-xl overflow-auto whitespace-pre-wrap leading-6 [&:has(p+p)>p]:ps-4 [&:has(p+p)>p]:-indent-4"
       >
         {skill.rules.split("\n").map((text, i) => (
           <p key={i}>{text}</p>
