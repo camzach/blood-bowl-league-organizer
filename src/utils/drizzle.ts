1023bab937d34e6e1055be4fb9badf97b84799b7
camzach <22503898+camzach@users.noreply.github.com>
2024-04-17 09:06:23 -0400

    setup for testing with local db

--- src/utils/drizzle.ts
+++ src/utils/drizzle.ts
@@ -1,11 +1,24 @@
 import * as schema from "../db/schema";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import { Pool } from "@neondatabase/serverless";
+import { neonConfig, Pool } from "@neondatabase/serverless";
 
 // @ts-expect-error typescript issues but it's chill
 let db: ReturnType<typeof drizzle<typeof schema>> = global.db;
 
 if (!db) {
+  if (!process.env.VERCEL_ENV) {
+    // Set the WebSocket proxy to work with the local instance
+    neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+    // Disable all authentication and encryption
+    neonConfig.useSecureWebSocket = false;
+    neonConfig.pipelineTLS = false;
+    neonConfig.pipelineConnect = false;
+  }
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
   const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
   db = drizzle(pool, {
     schema,
