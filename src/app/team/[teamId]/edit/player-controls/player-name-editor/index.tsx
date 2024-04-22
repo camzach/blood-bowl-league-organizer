9949d0a490686c28cbb59869383a83c2c981283b
camzach <22503898+camzach@users.noreply.github.com>
2024-04-22 14:35:06 -0400

    reduce component nesting for player name editor

--- src/app/team/[teamId]/edit/player-controls/player-name-editor/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentProps } from "react";
-import BaseComponent from "./player-name-editor";
-import { update } from "../actions";
-
-export default function PlayerNameEditor(
-  props: Omit<ComponentProps<typeof BaseComponent>, "update">
-) {
-  return <BaseComponent {...props} update={update} />;
-}
