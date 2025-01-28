bccba479e3a8b5c86cf50c596cd92a5e593d83b0
camzach <22503898+camzach@users.noreply.github.com>
2025-01-28 00:02:27 -0500

    fix baseurl

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,6 +1,6 @@
 import { adminClient, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
+  baseURL: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`,
   plugins: [adminClient(), organizationClient()],
 });
