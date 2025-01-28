3f03afebbd5faac523c44b4b9d66b5b71beb7d72
camzach <22503898+camzach@users.noreply.github.com>
2025-01-27 23:55:05 -0500

    change baseURL

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -1,6 +1,6 @@
 import { adminClient, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // the base url of your auth server
+  baseURL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
   plugins: [adminClient(), organizationClient()],
 });
