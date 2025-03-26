375c2806e2e997f5273e87e881c180c02510bcb6
camzach <22503898+camzach@users.noreply.github.com>
2025-03-26 10:33:43 -0400

    add calendar endpoint

---
+++ src/app/api/calendar/[leagueId]/calendar.ics/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest } from "next/server";
+import ical from "ical-generator";
+import { db } from "utils/drizzle";
+import { league as dbLeague } from "db/schema";
+import { eq } from "drizzle-orm";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ leagueId: string }> },
+) {
+  const league = await db.query.league.findFirst({
+    where: eq(dbLeague.id, (await params).leagueId),
+    with: {
+      seasons: {
+        with: {
+          roundRobinGames: {
+            with: {
+              game: {
+                with: {
+                  homeDetails: { with: { team: true } },
+                  awayDetails: { with: { team: true } },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  if (!league) return new Response("No calendar found");
+
+  const cal = ical({ name: "Blood Bowl" });
+  league.seasons.forEach((season) =>
+    season.roundRobinGames.forEach((game) => {
+      if (!game.game.scheduledTime) return;
+      cal.createEvent({
+        summary: `${game.game.homeDetails?.team.name ?? "TBD"} @ ${game.game.awayDetails?.team.name ?? "TBD"}`,
+        start: game.game.scheduledTime,
+        end: new Date(game.game.scheduledTime.valueOf() + 3 * 60 * 60 * 1000),
+      });
+    }),
+  );
+
+  return new Response(cal.toString(), {
+    headers: {
+      "Content-Type": "text/calendar; charset=utf-8",
+      "Content-Disposition": "attachment; filename=calendar.ics",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
