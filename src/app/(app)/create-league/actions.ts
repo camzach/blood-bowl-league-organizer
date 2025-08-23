baa19fc520d7ffbb76dcc46e4e332b7d30c2d411
camzach <22503898+camzach@users.noreply.github.com.>
2025-08-23 16:50:16 -0400

    use auth api instead of db calls for league creation

--- src/app/(app)/create-league/actions.ts
+++ src/app/(app)/create-league/actions.ts
@@ -1,10 +1,7 @@
 "use server";
 
 import { auth } from "~/auth";
-import { db } from "~/utils/drizzle";
-import nanoid from "~/utils/nanoid";
 import { redirect } from "next/navigation";
-import { league, member } from "~/db/schema/auth";
 import { headers } from "next/headers";
 
 export async function createLeague(leagueName: string) {
@@ -13,23 +10,12 @@ export async function createLeague(leagueName: string) {
     return redirect("/login");
   }
 
-  const newLeagueId = nanoid();
-
-  await db.transaction(async (tx) => {
-    await tx.insert(league).values({
-      id: newLeagueId,
+  await auth.api.createOrganization({
+    headers: await headers(),
+    body: {
       name: leagueName,
       slug: leagueName.toLowerCase().replace(/\s/g, "-"),
-      createdAt: new Date(),
-    });
-
-    await tx.insert(member).values({
-      id: nanoid(),
-      userId: session.user.id,
-      leagueId: newLeagueId,
-      role: "admin",
-      createdAt: new Date(),
-    });
+    },
   });
 
   return redirect("/");
