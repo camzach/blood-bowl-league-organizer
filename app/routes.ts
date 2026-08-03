449cc17f245475d931278bf853427338ba2776c2
camzach <22503898+camzach@users.noreply.github.com.>
2026-08-03 13:03:19 -0400

    home page

--- app/routes.ts
+++ app/routes.ts
@@ -14,16 +14,27 @@ export default [
     route("/team/new/create", "./routes/team/new/create.action.ts"),
     layout("./routes/team/loader.tsx", [
       route("/team/:teamId", "./routes/team/view/page.tsx"),
-      // Team edit action routes
-      layout("./routes/team/team-permission-middleware.ts", [ // <- this middleware needs to be created
+      layout("./routes/team/team-permission-middleware.ts", [
         route("/team/:teamId/edit", "./routes/team/edit/page.tsx"),
         route("/team/:teamId/edit/staff", "./routes/team/edit/staff.action.ts"),
-        route("/team/:teamId/edit/hire-player", "./routes/team/edit/hire-player.action.ts"),
+        route(
+          "/team/:teamId/edit/hire-player",
+          "./routes/team/edit/hire-player.action.ts",
+        ),
         route("/team/:teamId/edit/state", "./routes/team/edit/state.action.ts"),
-        route("/team/:teamId/edit/player/:playerId/update", "./routes/team/edit/player/$playerId/update.action.ts"),
-        route("/team/:teamId/edit/player/:playerId/fire", "./routes/team/edit/player/$playerId/fire.action.ts"),
-        route("/team/:teamId/edit/player/:playerId/advance", "./routes/team/edit/player/$playerId/advance.action.ts"),
-      ])
+        route(
+          "/team/:teamId/edit/player/:playerId/update",
+          "./routes/team/edit/player/$playerId/update.action.ts",
+        ),
+        route(
+          "/team/:teamId/edit/player/:playerId/fire",
+          "./routes/team/edit/player/$playerId/fire.action.ts",
+        ),
+        route(
+          "/team/:teamId/edit/player/:playerId/advance",
+          "./routes/team/edit/player/$playerId/advance.action.ts",
+        ),
+      ]),
     ]),
 
     route("/codex/skills", "./routes/codex/skills/page.tsx"),
